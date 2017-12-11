d267b235a859e4982feef07a7f2e85730cd285ef
Jérémy Jousse <jeremy.jousse@decathlon.com>
2017-12-11 15:47:07 +0100

    Second commit

---
+++ src/main/java/com/decathlon/stringtest/StringtestApplication.java
@@ -0,0 +1,12 @@
+package com.decathlon.stringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringtestApplication.class, args);
+	}
+}
