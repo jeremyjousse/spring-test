7a94994e2bb565d9390bac5771321cc1b5d7b33d
Jérémy Jousse <jeremy.jousse@decathlon.com>
2017-12-12 09:11:20 +0100

    ADD First Controllers

---
+++ src/main/java/com/decathlon/stringtest/UserRepository.java
@@ -0,0 +1,11 @@
+package com.decathlon.stringtest;
+
+import org.springframework.data.repository.CrudRepository;
+
+// This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
+// CRUD refers Create, Read, Update, Delete
+
+public interface UserRepository extends CrudRepository<User, Long> {
+
+    User findById(Integer id);
+}
